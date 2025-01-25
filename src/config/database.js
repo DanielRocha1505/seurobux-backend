f91795a2818af6407c768655e63b6cc5bcb01f46
Daniel Rocha <danielrocha1507@gmail.com>
2025-01-25 01:54:14 -0300

    [+] Inclusao base projeto v2

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'sua_database',
+  host: process.env.DB_HOST || 'srv1781.hstgr.io',
+  user: process.env.DB_USER || 'u883675686_tstdev44',
+  password: process.env.DB_PASS || 'tstdev44',
+  database: process.env.DB_NAME || 'u883675686_tstdev44',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
