f138d6b6a257d0b7cc9a896d19dd914a61fd240f
Daniel Rocha <danielrocha1507@gmail.com>
2025-02-01 08:59:25 -0300

    [+] Teste

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./src/routes');
 
 const app = express();
 
